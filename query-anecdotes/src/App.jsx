c14c0dbc87d1ba0461a2de2814021bd07ba934f5
djakimiuk <63128911+djakimiuk@users.noreply.github.com>
2023-10-03 19:28:49 +0200

    Exercise 6.21 and 6.22 solved

--- query-anecdotes/src/App.jsx
+++ query-anecdotes/src/App.jsx
@@ -1,23 +1,41 @@
 import AnecdoteForm from "./components/AnecdoteForm";
 import Notification from "./components/Notification";
-import { useQuery } from "@tanstack/react-query";
-import { getAnecdotes } from "./requests";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getAnecdotes, updateAnecdote } from "./requests";
 
 const App = () => {
+  const queryClient = useQueryClient();
+  const updateAnecdoteMutation = useMutation(updateAnecdote, {
+    onSuccess: (updatedAnecdote) => {
+      const anecdotes = queryClient.getQueryData(["anecdotes"]);
+      console.log(anecdotes);
+      const updatedAnecdotes = anecdotes.map((anecdote) =>
+        anecdote.id !== updatedAnecdote.id ? anecdote : updatedAnecdote
+      );
+      console.log(updatedAnecdotes);
+      queryClient.setQueryData(["anecdotes"], updatedAnecdotes);
+    },
+  });
+
   const handleVote = (anecdote) => {
-    console.log("vote");
+    updateAnecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 });
   };
 
   const result = useQuery({
     queryKey: ["anecdotes"],
     queryFn: getAnecdotes,
     retry: 1,
+    refetchOnWindowFocus: false,
   });
 
   console.log(JSON.parse(JSON.stringify(result)));
 
   if (result.isLoading) {
     return <div>anecdote service not available due to problems in server</div>;
+  }
+
+  if (result.isError) {
+    return <div>Error: {result.error.message}</div>;
   }
 
   const anecdotes = result.data;
--- query-anecdotes/src/requests.js
+++ query-anecdotes/src/requests.js
@@ -5,3 +5,18 @@ const baseUrl = "http://localhost:3001/anecdotes";
 export const getAnecdotes = () => {
   return axios.get(baseUrl).then((res) => res.data);
 };
+
+export const createAnecdote = (newAnecdote) => {
+  if (newAnecdote.content.length < 5) {
+    return Promise.reject(
+      new Error(`Content length must be at least 5 charcters!`)
+    );
+  }
+  return axios.post(baseUrl, newAnecdote).then((res) => res.data);
+};
+
+export const updateAnecdote = (updatedAnecdote) => {
+  return axios
+    .put(`${baseUrl}/${updatedAnecdote.id}`, updatedAnecdote)
+    .then((res) => res.data);
+};
